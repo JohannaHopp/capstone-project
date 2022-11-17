7fe543673fa960ce99432f2c7367fc21ecbc7524
Johanna Hopp <johannahopp96@web.de>
2022-11-17 12:03:10 +0100

    handle onSubmit

--- components/NameAccountForm.js
+++ components/NameAccountForm.js
@@ -1,34 +1,40 @@
-import { useState } from "react";
+import { createElement, useState } from "react";
 
 export default function NameAccountForm() {
-  const [username, setUsername] = useState("");
-  const [account, setAccount] = useState("");
+  const [input, setInput] = useState({
+    name: "",
+    kontostand: "",
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setInput((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(input);
+  };
 
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Bitte verrate uns deinen Namen:</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          required
-        />
+        <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
         <label htmlFor="kontostand">und jetzt gib bitte deinen Kontostand an:</label>
         <input
           type="number"
           name="kontostand"
           placeholder="Kontostand (in €)"
-          value={account}
-          onChange={(event) => setAccount(event.target.value)}
+          onChange={handleChange}
           required
         />
-        <button type="submit">submit</button>
+        <button type="submit">bestätigen</button>
       </form>
-      <p>{username}</p>
-      <p>{account}</p>
     </>
   );
 }

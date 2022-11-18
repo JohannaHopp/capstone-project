82d132eb7e163d19bf333587c6dfa358996f4dc3
Johanna Hopp <johannahopp96@web.de>
2022-11-18 16:32:18 +0100

    write more tests

--- components/NameAccountForm.test.js
+++ components/NameAccountForm.test.js
@@ -1,10 +1,26 @@
+import { userEvent } from "@storybook/testing-library";
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import NameAccountForm from "./NameAccountForm";
 
 describe("NameAccountForm", () => {
-  it("should render", () => {
+  it("should load the page", () => {
     render(<NameAccountForm />);
   });
+
+  it("type name in name input", () => {
+    render(<NameAccountForm />);
+
+    userEvent.type(screen.getByLabelText(/name/i), "Max Mustermann");
+  });
+
+  it("type kontostand in kontostand input", () => {
+    render(<NameAccountForm />);
+
+    userEvent.type(screen.getByLabelText(/kontostand/i), "4325");
+  });
+
+  it.todo("only numbers in kontostand input");
+  it.todo("submit button works");
 });

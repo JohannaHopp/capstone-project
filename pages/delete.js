538a21a7630f4c179bef74206099febfd4059310
Johanna Hopp <johannahopp96@web.de>
2022-12-09 14:47:16 +0100

    add simple styling

--- pages/delete.js
+++ pages/delete.js
@@ -1,4 +1,5 @@
 import Footer from "../components/Footer";
+import styled from "styled-components";
 
 export default function Delete() {
   function handleClick() {
@@ -7,10 +8,46 @@ export default function Delete() {
   }
 
   return (
-    <main>
-      <p>Bist du sicher, dass du alles löschen möchtest?</p>
-      <button onClick={handleClick}>Ja, alles löschen</button>
+    <StyledMain>
+      <StyledParagraph>
+        Bist du sicher,
+        <br /> dass du <br />
+        <StyledSpan>alles löschen</StyledSpan>
+        <br /> möchtest?
+      </StyledParagraph>
+      <StyledButton onClick={handleClick}>alles löschen</StyledButton>
       <Footer />
-    </main>
+    </StyledMain>
   );
 }
+
+const StyledMain = styled.main`
+  display: grid;
+  flex-direction: column;
+`;
+
+const StyledParagraph = styled.p`
+  justify-content: center;
+  font-size: 3em;
+  text-align: center;
+`;
+
+const StyledSpan = styled.span`
+  color: red;
+`;
+
+const StyledButton = styled.button`
+  width: 140px;
+  height: 30px;
+  text-align: center;
+  font-size: 1.3em;
+  border-radius: 5px;
+  background-color: var(--red-button);
+  color: black;
+  z-index: 1;
+  border: 1px solid black;
+  position: absolute;
+  bottom: 28px;
+  right: 0px;
+  margin: 1em;
+`;
